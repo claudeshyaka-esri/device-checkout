a98891422fd4a92e151c8fbe84ad56902b82d419
Jose Banuelos <JBanuelos@esri.com>
2020-06-11 19:30:29 -0700

    modifying the boilerplate to test React components

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// react strap components
+import { Button } from "reactstrap";
+
+const App = () => {
+  const buttonText = "Click Me!";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label className="label" htmlFor="name">
+        This is a label
+      </label>
+      <input id="name" type="text" />
+      <Button color="primary" size="lg">
+        {buttonText}
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// components
+import App from "./App";
+
+// take the react component and render on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
