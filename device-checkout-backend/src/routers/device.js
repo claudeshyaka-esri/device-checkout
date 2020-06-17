cc50ca23726e3fb6afea244e69831f80f44d73b8
Claude Shyaka <claudeshyaka@Claudes-MacBook-Pro.local>
2020-06-16 20:20:50 -0400

    added device reading, updating and deletion rest endpoints.

--- device-checkout-backend/src/routers/device.js
+++ device-checkout-backend/src/routers/device.js
@@ -6,7 +6,7 @@ const router = new express.Router()
 // device creation endpoint
 router.post('/devices', async (req, res) => {
     const device = new Device(req.body)
-    
+
     try {
         await device.save()
         res.status(201).send(device)
@@ -20,6 +20,41 @@ router.get('/devices', async (req, res) => {
     try {
         const devices = await Device.find({})
         res.send(devices)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// device reading endpoint
+router.get('/devices/:id', async (req, res) => {
+    try {
+        const device = await Device.findById(req.params.id)
+
+        if(!device){
+            res.status(404).send()
+        }
+
+        res.send(device)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// device updating endpoint
+router.patch('devices/:id', async (req, res) => {
+    res.send('update device endpoint.')
+})
+
+// device deletion endpoint
+router.delete('/devices/:id', async (req, res) => {
+    try {
+        const device = await Device.findByIdAndDelete(req.params.id)
+        
+        if(!device){
+            return res.status(404).send()
+        }
+
+        res.send(device)
     } catch (e) {
         res.status(500).send()
     }
