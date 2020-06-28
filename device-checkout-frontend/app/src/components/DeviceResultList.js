9151b710d148e5466149ef87a6f659ffe263a453
Jose Banuelos <JBanuelos@esri.com>
2020-06-28 10:17:22 -0700

    added search suggestions to search bar

--- device-checkout-frontend/app/src/components/DeviceResultList.js
+++ device-checkout-frontend/app/src/components/DeviceResultList.js
@@ -2,12 +2,9 @@ import React from "react";
 import DeviceCard from "./DeviceCard";
 
 const DeviceResultList = (props) => {
+  let counter = 0;
   const devices = props.devices.map((device) => {
-    return (
-      <div>
-        <DeviceCard deviceName={device.name} />
-      </div>
-    );
+    return <DeviceCard deviceName={device.name} key={counter++} />;
   });
   return <div>{devices}</div>;
 };
---
+++ device-checkout-frontend/app/src/components/SearchBar.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import SearchSuggestions from "./SearchSuggestions";
+
+class SearchBar extends React.Component {
+  state = { searchTerm: "", filteredSearch: [] };
+
+  onSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log("search submitted");
+    console.log(this.props.source);
+    // call the callback from parent component
+  };
+
+  onInputChange = (event) => {
+    const { source } = this.props;
+    let filter, textValue, suggestStr;
+
+    this.setState(
+      {
+        searchTerm: event.target.value,
+      },
+      () => {
+        textValue = this.state.searchTerm.toUpperCase();
+        const filteredResults = [];
+        source.forEach((name) => {
+          if (name.toUpperCase().includes(textValue)) {
+            filteredResults.push(name);
+          }
+        });
+        this.setState({
+          filteredSearch: filteredResults,
+        });
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="ui search-bar">
+        <form onSubmit={this.onSearchSubmit} className="ui form">
+          <div className="ui icon input" style={{ width: "75%" }}>
+            <input
+              type="text"
+              placeholder="Search for device..."
+              value={this.state.searchTerm}
+              onChange={this.onInputChange}
+            />
+            <i className="search icon"></i>
+          </div>
+        </form>
+        <div style={{ width: "75%" }}>
+          {this.state.searchTerm.length > 0 ? (
+            <SearchSuggestions source={this.state.filteredSearch} />
+          ) : (
+            <SearchSuggestions source={[]} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
